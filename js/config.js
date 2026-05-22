3fdac49efeefdd862534e37c1a9d1cb8760a4b59
ahmadz02 <129569697+ahmadz02@users.noreply.github.com>
2026-05-22 14:44:00 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Replace these with your Supabase Project URL and anon public key.
 window.WQ_CONFIG = {
-  export const SUPABASE_URL = "https://ppcxuptjzhrqkcdokryk.supabase.co",
-  export const SUPABASE_ANON_KEY ='sb_publishable_kDTuxJSCyWeIxEa-1c3Aug_-Cs5S7Fq',
+  SUPABASE_URL: "https://ppcxuptjzhrqkcdokryk.supabase.co",
+  SUPABASE_ANON_KEY: 'sb_publishable_kDTuxJSCyWeIxEa-1c3Aug_-Cs5S7Fq',
   STORAGE_BUCKET: 'wealth-quadrant'
 };
